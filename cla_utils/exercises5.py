8678a18f49b257c19d4862e81e8a758589df79cf
Cristian Ursache <cristi.ursache00@gmail.com>
2021-11-29 02:34:25 +0000

    backwards stability tests

--- cla_utils/exercises5.py
+++ cla_utils/exercises5.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy import random
+from cla_utils.exercises3 import householder_solve
 
 
 def randomQ(m):
@@ -9,7 +11,7 @@ def randomQ(m):
     
     :return Q: the mxm numpy array containing the orthogonal matrix.
     """
-    Q, R = linalg.qr(random.randn(m, m))
+    Q, R = np.linalg.qr(random.randn(m, m))
     return Q
 
 
@@ -23,7 +25,7 @@ def randomR(m):
     """
     
     A = random.randn(m, m)
-    return numpy.triu(A)
+    return np.triu(A)
 
 
 def backward_stability_householder(m):
@@ -38,7 +40,11 @@ def backward_stability_householder(m):
         Q1 = randomQ(m)
         R1 = randomR(m)
 
-        raise NotImplementedError
+        Q2, R2 = np.linalg.qr(Q1 @ R1)
+
+        print('||Q2 - Q1|| = {}'.format(np.linalg.norm(Q2 - Q1)))
+        print('||R2 - R1|| = {}'.format(np.linalg.norm(R2 - R1)))
+        print('||A - Q2R2|| = {}'.format(np.linalg.norm(Q1 @ R1 - Q2 @ R2)))
 
 
 def solve_R(R, b):
@@ -51,8 +57,15 @@ def solve_R(R, b):
 
     :param x: an m-dimensional numpy array
     """
-                     
-    raise NotImplementedError
+
+    m = len(R)
+    x = np.zeros(m)
+
+    x[-1] = b[-1] / R[-1, -1]
+    for i in range(m-2, -1, -1):
+        x[i] = (b[i] - np.dot(R[i, i+1:], x[i+1:])) / R[i, i]
+    
+    return x
 
 
 def back_stab_solve_R(m):
@@ -67,7 +80,12 @@ def back_stab_solve_R(m):
         A = random.randn(m, m)
         R = np.triu(A)
 
-        raise NotImplementedError
+        x = random.randn(m)
+        y = R @ x
+
+        x_tilde = solve_R(R, y)
+
+        print(np.linalg.norm(R @ x_tilde - y) / np.linalg.norm(x_tilde - x))
 
 
 def back_stab_householder_solve(m):
@@ -77,4 +95,13 @@ def back_stab_householder_solve(m):
 
     :param m: the matrix dimension parameter.
     """
-    raise NotImplementedError
+    for k in range(20):
+        A = random.randn(m, m)
+        x = random.randn(m)
+
+        b = A @ x
+        b = b.reshape((m, 1))
+
+        x_tilde = householder_solve(A, b)
+
+        print(np.linalg.norm(A @ x_tilde - b) / np.linalg.norm(x_tilde - x))
