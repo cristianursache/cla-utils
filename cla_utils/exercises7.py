938b51a7f75351e7f193dec020ed6b7c5b87fae0
Cristian Ursache <cristi.ursache00@gmail.com>
2021-12-01 13:03:12 +0000

    implemented LUP_inplace

--- cla_utils/exercises7.py
+++ cla_utils/exercises7.py
@@ -7,6 +7,9 @@ def perm(p, i, j):
     P_{i,j} exchanges rows i and j.
 
     :param p: an m-dimensional numpy array of integers.
+
+    :return pij: an m-dimensional integer array describing the permutation \
+    i.e. (Px)[i] = x[p[i]]
     """
 
     pij = p
@@ -25,11 +28,26 @@ def LUP_inplace(A):
 
     :param A: an mxm-dimensional numpy array
 
-    :return p: an m-dimensional integer array describing the permutation \
-    i.e. (Px)[i] = x[p[i]]
+    :return p: an m-dimensional integer array describing the permutation
+    associated with the decomposition PA = LU.
     """
                      
-    raise NotImplementedError
+    m = len(A)
+    p = [i for i in range(m)]
+
+    for k in range(m-1):
+        i = np.argmax(abs(A[k:, k])) + k
+        A[[i, k]] = A[[k, i]]
+        p = perm(p, i, k)
+        lk = np.zeros(m)
+        lk[k+1:] = -A[k+1:, k] / A[k, k]
+        ek = np.zeros(m)
+        ek[k] = 1
+        outer = np.outer(lk, ek)
+        A[k:, k:] += outer[k:, k:] @ A[k:, k:]
+        A -= outer
+
+    return p
 
 
 def solve_LUP(A, b):
