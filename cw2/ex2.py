7513da6cc89d62baebc0376e1637997225cfddc6
Cristian Ursache <cristi.ursache00@gmail.com>
2021-12-06 11:05:39 +0000

    ex2 final

--- cw2/ex2.py
+++ cw2/ex2.py
@@ -3,7 +3,12 @@ import numpy as np
 
 def evals(eps):
     """
-    
+    Given a real number epsilon, computes the eigenvalues of the
+    perturbed identity matrix A_epsilon.
+
+    :param eps: a real number
+
+    :return: a 2-dimensional numpy array containing the eigenvalues
     """
     tr = 2 + eps
     det = 1 + eps
@@ -13,9 +18,14 @@ def evals(eps):
     l1 = (tr + np.sqrt(delta)) / 2
     l2 = (tr - np.sqrt(delta)) / 2
 
-    return l1, l2
+    return np.array([l1, l2])
 
 eps = 10 ** -14
 
 print(evals(0))
 print(evals(eps))
+
+
+true_lambda = np.array([1, 1 + eps])
+
+print(np.linalg.norm(true_lambda - evals(eps)))
