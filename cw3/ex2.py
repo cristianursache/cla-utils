cd29c17227c27a715deaa5f76baec36f52fd3058
Cristian Ursache <cristi.ursache00@gmail.com>
2022-01-12 20:21:35 +0000

    ex2 final - no plots

--- cw3/ex2.py
+++ cw3/ex2.py
@@ -1,24 +1,41 @@
 import numpy as np
 from cla_utils import hessenberg, pure_QR
-import matplotlib.pyplot as plt
 
 def createAij(n):
     i, j = np.indices((n, n))
     A = 1 / (1 + i + j)
     return A
 
+# Eigenvalues of A using np.linalg.eig
 A = createAij(5)
 print(np.linalg.eig(A)[0])
 
+# Eigenvalues of A using the new termination condition
 hessenberg(A)
 Ak = pure_QR(A, 10000, tridiagonal=True)[0]
 print(np.diag(Ak, 0))
 
 def modified_QR(A, maxit, shifted=False):
-    e = []
-    off = []
+    """
+    Given a symmetric matrix A, applies the modified QR algorithm
+    (shifted or not shifted) and returns the result.
+
+    :param A: an mxm-dimensional numpy array (must be symmetric)
+    :param maxit: an integer, maximum number of iterations
+    :param shifted: logical, if True applies the shifted
+    QR algorithm
+
+    :return e: an m-dimensional numpy array containing the
+    eigenvalues of A
+    :return off: a numpy array containing the values of |T_{k, k-1}|
+    """
+
     m = len(A)
     hessenberg(A)
+
+    e = []
+    off = []
+
     for k in range(m-1, -1, -1):
         if k != 0:
             Ak, off_k = pure_QR(A, maxit, tridiagonal=True, shifted=shifted)
@@ -28,15 +45,13 @@ def modified_QR(A, maxit, shifted=False):
             off.extend(off_k)
         e.append(Ak[k, k])
         A = Ak[:k, :k]
+
     return e, off
 
+# Eigenvalues of A using the modified QR algoritm
 A = createAij(5)
 print(modified_QR(A, 5000)[0])
 
-A = createAij(5)
-#plt.figure()
-#plt.plot(modified_QR(A, 5000)[1])
-#plt.yscale('log')
-
+# Eigenvalues of A using the shifted modified QR algorithm
 A = createAij(5)
 print(modified_QR(A, 5000, shifted=True)[0])
