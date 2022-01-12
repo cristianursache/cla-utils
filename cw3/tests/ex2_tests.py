2c870cb3ed08c1091d2e1b4bc6b004ec4b8c7eac
Cristian Ursache <cristi.ursache00@gmail.com>
2022-01-12 20:01:14 +0000

    ex2 tests all pass

--- cw3/tests/ex2_tests.py
+++ cw3/tests/ex2_tests.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from numpy import random
 from cw3.ex2 import createAij, modified_QR
 from cla_utils import hessenberg, pure_QR
 
@@ -13,10 +14,11 @@ def test_pure_QR_tridiag(n):
     for eval in e:
         assert np.abs(np.linalg.det(A0 - eval * np.eye(n))) < 1e-06
 
-@pytest.mark.parametrize('n', [5])
+@pytest.mark.parametrize('n', [5, 7, 10])
 def test_modified_QR(n):
-    A = createAij(n)
+    R = random.randn(n, n)
+    A = R + R.T
     A0 = 1.0 * A
     e = modified_QR(A, 10000)[0]
     for eval in e:
-        assert np.abs(np.linalg.det(A0 - eval * np.eye(n))) < 1e-06
+        assert np.abs(np.linalg.det(A0 - eval * np.eye(n))) < 1e-04
