bf56c86c83a9571430ebe98a5bb97fa253654a1f
Cristian Ursache <cristi.ursache00@gmail.com>
2022-01-12 17:00:37 +0000

    tests for modified QR

--- cw3/tests/ex2_tests.py
+++ cw3/tests/ex2_tests.py
@@ -1,13 +1,22 @@
 import pytest
 import numpy as np
-from cw3.ex2 import createAij
+from cw3.ex2 import createAij, modified_QR
 from cla_utils import hessenberg, pure_QR
 
 @pytest.mark.parametrize('n', [5])
-def test_mod_QR(n):
+def test_pure_QR_tridiag(n):
     A = createAij(n)
-    e = np.linalg.eig(A)[0]
+    A0 = 1.0 * A
     hessenberg(A)
-    Ak = pure_QR(A, 10000, tridiagonal=True)
-    e_QR = np.diag(Ak, 0)
-    assert np.linalg.norm(e - e_QR) < 1e-06
+    Ak = pure_QR(A, 10000, tridiagonal=True)[0]
+    e = np.diag(Ak, 0)
+    for eval in e:
+        assert np.abs(np.linalg.det(A0 - eval * np.eye(n))) < 1e-06
+
+@pytest.mark.parametrize('n', [5])
+def test_modified_QR(n):
+    A = createAij(n)
+    A0 = 1.0 * A
+    e = modified_QR(A, 10000)[0]
+    for eval in e:
+        assert np.abs(np.linalg.det(A0 - eval * np.eye(n))) < 1e-06
